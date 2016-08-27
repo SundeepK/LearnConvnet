87788e216bed46c403d788626ff3fb57b4791aa2
SundeepK <sundeepkahlon@hotmail.com>
2016-08-27 22:45:41 +0100

    Fix bug where the wrong y value was being used to get the initial indexes.

--- conv/src/poolLayer.py
+++ conv/src/poolLayer.py
@@ -17,13 +17,11 @@ class PoolLayer(object):
         out_y = math.floor((input_y - self.spatial_extent) / self.stride) + 1
 
         # Get Starting block indices
-        start_idx = (numpy.arange(input_x)[:,None]*input_x + numpy.arange(self.spatial_extent))
+        start_idx = (numpy.arange(self.spatial_extent)[:,None]*input_x + numpy.arange(self.spatial_extent))
 
         # Get off-setted indices across the height and width of input array
         offset_idx = (numpy.arange(0, row_extent, self.stride)[:,None]*input_x + numpy.arange(0, col_extent, self.stride))
-
-        all_indexes = numpy.hstack(start_idx.ravel()[:, None] + offset_idx.ravel())
-
+        all_indexes = (start_idx.ravel()[:, None] + offset_idx.ravel())
         pooled_out = numpy.empty(len(inputs), dtype=object)
         for f in range(0, len(inputs)):
             filter_map = inputs[f]
