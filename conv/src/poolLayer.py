0d5cb238f9ba8f5b88ca15c24b6263e4f711a9ba
Sundeep <kahlon.sundeep@gmail.com>
2016-11-07 22:53:34 +0000

    Takes in ConvMatrix and return ConvMatrix.

--- conv/src/poolLayer.py
+++ conv/src/poolLayer.py
@@ -8,8 +8,8 @@ class PoolLayer(object):
         self.stride = stride
         self.spatial_extent = spatial_extent
 
-    def forward(self, inputs):
-        input_y, input_x = inputs[0].shape
+    def forward(self, input):
+        input_z, input_y, input_x = input.params.shape
         col_extent = input_x - self.spatial_extent + 1
         row_extent = input_y - self.spatial_extent + 1
 
@@ -22,13 +22,13 @@ class PoolLayer(object):
         # Get off-setted indices across the height and width of input array
         offset_idx = (numpy.arange(0, row_extent, self.stride)[:,None]*input_x + numpy.arange(0, col_extent, self.stride))
         all_indexes = (start_idx.ravel()[:, None] + offset_idx.ravel())
-        pooled_out = numpy.empty(len(inputs), dtype=object)
-        for f in range(0, len(inputs)):
-            filter_map = inputs[f]
+        pooled_out = numpy.empty((input_z, out_y, out_x), dtype=object)
+        for f in range(0, input_z):
+            filter_map = input.params[f]
             pooled_values = numpy.amax(filter_map.take(all_indexes), axis=0)
             pooled_out[f] = pooled_values.reshape(out_x, out_y)
 
-        return pooled_out
+        return ConvMatrix(input_z, out_y, out_x, pooled_out)
 
     def get_params_and_grads(self):
         return []
