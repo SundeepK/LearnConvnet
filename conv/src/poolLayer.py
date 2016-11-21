b9ed2b4754de84f4dc2d1428f755b6ce036ab4f7
Sundeep <kahlon.sundeep@gmail.com>
2016-11-21 23:48:57 +0000

    Change pool layer to do back prop.

--- conv/src/poolLayer.py
+++ conv/src/poolLayer.py
@@ -8,8 +8,9 @@ class PoolLayer(object):
         self.stride = stride
         self.spatial_extent = spatial_extent
 
-    def forward(self, input):
-        input_z, input_y, input_x = input.params.shape
+    def forward(self, input_matrix):
+        self.input = input_matrix
+        input_z, input_y, input_x = input_matrix.params.shape
         col_extent = input_x - self.spatial_extent + 1
         row_extent = input_y - self.spatial_extent + 1
 
@@ -22,16 +23,23 @@ class PoolLayer(object):
         # Get off-setted indices across the height and width of input array
         offset_idx = (numpy.arange(0, row_extent, self.stride)[:,None]*input_x + numpy.arange(0, col_extent, self.stride))
         all_indexes = (start_idx.ravel()[:, None] + offset_idx.ravel())
-        pooled_out = numpy.empty((input_z, out_y, out_x), dtype=object)
+        pooled_out = numpy.empty((input_z, out_y, out_x))
+        self.pooled_out_max_indices = numpy.empty((input_z, out_y, out_x))
         for f in range(0, input_z):
-            filter_map = input.params[f]
-            pooled_values = numpy.amax(filter_map.take(all_indexes), axis=0)
+            filter_map = input_matrix.params[f]
+            selected_vales = filter_map.take(all_indexes)
+            pooled_values = numpy.amax(selected_vales, axis=0)
             pooled_out[f] = pooled_values.reshape(out_x, out_y)
+            self.pooled_out_max_indices[f] = numpy.diagonal(numpy.take(all_indexes, numpy.argmax(selected_vales, axis=0), axis=0)).reshape(out_x, out_y)
 
-        return ConvMatrix(input_z, out_y, out_x, pooled_out)
+        self.out = ConvMatrix(input_z, out_y, out_x, pooled_out)
+        return self.out
 
     def backwards(self, y):
-        pass
+        out_z, out_y, out_x = self.pooled_out_max_indices.shape
+        for f in range(0, out_z):
+            chained = numpy.take(self.input.grads[f], self.pooled_out_max_indices[f]).reshape(out_y, out_x) + self.out.grads[f]
+            numpy.put(self.input.grads[f], self.pooled_out_max_indices[f], chained)
 
 
     def get_params_and_grads(self):
--- conv/tests/poolLayerTest.py
+++ conv/tests/poolLayerTest.py
@@ -11,9 +11,15 @@ class PoolLayerTest(unittest.TestCase):
         input = np.array([[[1, 1, 2, 4],
                            [5, 6, 7, 8],
                            [3, 2, 1, 0],
+                           [1, 2, 3, 4]],
+                          [[1, 1, 2, 4],
+                           [5, 1, 7, 8],
+                           [3, 2, 1, 0],
                            [1, 2, 3, 4]]])
 
         expected = np.array([[[6, 8],
+                              [3, 4]],
+                             [[5, 8],
                               [3, 4]]])
 
         undertest = PoolLayer(2, 2)
