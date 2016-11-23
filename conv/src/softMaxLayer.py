365d6eee3707a437a7e01f4977daf51033cba49e
Sundeep <kahlon.sundeep@gmail.com>
2016-11-23 22:32:33 +0000

    Fix softmax layer.

--- conv/src/softMaxLayer.py
+++ conv/src/softMaxLayer.py
@@ -6,29 +6,17 @@ class SoftmaxLayer(object):
 
     def forward(self, inputs):
         self.inputs = inputs
-        out_softmax = numpy.empty(inputs.x, dtype=object)
         max = numpy.max(inputs.params)
-        for f in range(0, inputs.x):
-            layer = inputs.params[f]
-            e_x = numpy.exp((layer - max).astype(float))
-            out_softmax[f] = e_x / e_x.sum()
-        self.out = out_softmax
-        print(inputs.params)
-        return out_softmax
+        e_x = numpy.exp((inputs.params - max).astype(float))
+        self.out = e_x / e_x.sum()
+        print(self.out)
+        return self.out
 
     def backwards(self, y):
-        dws = numpy.zeros(self.inputs.x)
-        dws[y] = 1
-        dws = -(dws - self.out)
-        self.dws = dws
-        return dws
-
-    def loss(self, y):
-        if self.dws:
-            return -(numpy.log(self.out))
-        else:
-            self.backwards(y)
-            return -(numpy.log(self.out))
+        self.inputs.grads = numpy.zeros(self.inputs.x)
+        self.inputs.grads[y] = 1
+        self.inputs.grads = -(self.inputs.grads - self.out)
+        return -(numpy.log(self.out[y]))
 
     def get_params_and_grads(self):
         return []
