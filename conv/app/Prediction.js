9d809806609e1b58fb48da664a6cedba946bae57
Sundeep <kahlon.sundeep@gmail.com>
2016-12-19 22:13:19 +0000

    Draw basic predictions.

--- conv/app/Prediction.js
+++ conv/app/Prediction.js
@@ -3,12 +3,18 @@ import React from "react"
 
 var Prediction = React.createClass({
     render: function() {
-        return (
-            <div className="prediction">
-                <img></img>
-                {/*{this.props.prediction}*/}
-            </div>
-        );
+        if(this.props.src) {
+            return (
+                <div className="prediction">
+                    <img src={this.props.src} width={this.props.width} height={this.props.height}/>
+                </div>
+            );
+        } else {
+            return (
+                <div className="prediction">
+                </div>
+            );
+        }
     }
 });
 
--- conv/app/Predictions.js
+++ conv/app/Predictions.js
@@ -2,20 +2,65 @@ import React from "react"
 import Prediction from './Prediction';
 
 
-var Predictions = React.createClass({
-    render: function() {
-        let items = [];
-        for (let i = 0; i < 253; i++) {
-            items[i] = <Prediction/>
-        }
+class Predictions extends React.Component{
 
+    constructor(props) {
+        super(props);
+        let pred = [];
+        for (var i = 0; i < 253; i++) {
+            pred[i] = {};
+        }
+        this.state = {
+            predictions: pred
+        };
+    }
+
+    componentDidMount() {
+        var ws = new WebSocket('ws://localhost:8888/ws');
+        ws.binaryType = "arraybuffer";
+        ws.onopen = function(){
+        };
+        ws.onmessage = this.onMessage.bind(this)
+    }
+
+    onMessage(evt){
+        if (typeof evt.data === "string") {
+            console.log(evt.data)
+        } else {
+            var imageWidth = 32, imageHeight = 32;
+            var blob = new Blob([(evt.data)], {type: 'image/jpeg'});
+            var url = (window.URL || window.webkitURL).createObjectURL(blob);
+            var image = {};
+            image.width = imageWidth;
+            image.height = imageHeight;
+            image.src = url;
+
+            const predictions = this.state.predictions;
+            if (predictions.length >= 253){
+                predictions.pop();
+            }
+            predictions.unshift(image);
+            this.setState({ predictions: predictions });
+        }
+    }
+
+    render() {
+        let items = [];
+        for (let i = this.state.predictions.length - 1; i > 0 ; i--) {
+            let img = this.state.predictions[i];
+            if (img.src) {
+                items[i] = <Prediction key={i} src={img.src} width={img.width} height={img.height}/>
+            } else {
+                items[i] = <Prediction key={i} />
+            }
+        }
         return (
             <div className="predictions">
                 {items}
             </div>
         );
     }
-});
+}
 
 export default Predictions
 
